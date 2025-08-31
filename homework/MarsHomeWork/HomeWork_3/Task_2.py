8688c0d0915c5ec8247831fd69d88e8516426054
Khusnutdinov Marsel <marsepta806@gmail.com>
2025-08-31 11:24:11 +0300

    Mars HomeWork 3 Done

---
+++ homework/MarsHomeWork/HomeWork_3/Task_2.py
@@ -0,0 +1,5 @@
+x = 20
+y = 10
+equation = x - y / 1 + x * y
+
+print(equation)
---
+++ homework/MarsHomeWork/HomeWork_3/Task_3.py
@@ -0,0 +1,8 @@
+a = 50
+b = 100
+
+arithmetic_mean = (a + b) / 2
+geometric_mean = (a * b) ** 0.5
+
+print('Среднее арифметическое =', arithmetic_mean)
+print('Среднее арифметическое =', geometric_mean)
