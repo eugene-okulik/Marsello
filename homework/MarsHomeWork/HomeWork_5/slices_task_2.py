5fd3b1c57d213fbc5f85a6f7f4a5f06cfdea99fb
Khusnutdinov Marsel <marsepta806@gmail.com>
2025-09-07 22:21:35 +0300

    Mars_Husnut_HomeWork_fixed_1

--- homework/MarsHomeWork/HomeWork_5/slices_task_2.py
+++ homework/MarsHomeWork/HomeWork_5/slices_task_2.py
@@ -1,26 +1,16 @@
-result_1 = 'результат операции: 42'
-result_2 = 'результат операции: 514'
-result_3 = 'результат работы программы: 9'
+text_1 = 'результат операции: 42'
+text_2 = 'результат операции: 514'
+text_3 = 'результат работы программы: 9'
 
-list_from_text_result_1 = result_1.split()  # делаем список из строки
-number_1 = int(list_from_text_result_1[-1]) + 10  # выполняем сложение
-list_from_text_result_1.pop(-1)  # удаляем старое значение
-list_from_text_result_1.append(str(number_1))  # подставляем новое
-list_from_text_result_1 = ' '.join(
-    list_from_text_result_1)  # конвертируем обратно в строку
+index_number_1 = text_1.index(':') + 2  # находим локатор отсчета
+result_1 = int(text_1[index_number_1:]) + 10  # производим вычисления
 
-list_from_text_result_2 = result_2.split()
-number_2 = int(list_from_text_result_2[-1]) + 10
-list_from_text_result_2.pop(-1)
-list_from_text_result_2.append(str(number_2))
-list_from_text_result_2 = ' '.join(list_from_text_result_2)
+index_number_2 = text_2.index(':') + 2
+result_2 = int(text_2[index_number_2:]) + 10
 
-list_from_text_result_3 = result_3.split()
-number_3 = int(list_from_text_result_3[-1]) + 10
-list_from_text_result_3.pop(-1)
-list_from_text_result_3.append(str(number_3))
-list_from_text_result_3 = ' '.join(list_from_text_result_3)
+index_number_3 = text_3.index(':') + 2
+result_3 = int(text_3[index_number_3:]) + 10
 
-print(list_from_text_result_1)
-print(list_from_text_result_2)
-print(list_from_text_result_3)
+print(result_1)
+print(result_2)
+print(result_3)
--- homework/MarsHomeWork/HomeWork_5/unpacking_list_task_3.py
+++ homework/MarsHomeWork/HomeWork_5/unpacking_list_task_3.py
@@ -1,10 +1,8 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-name_1, name_2, name_3 = students
-subjects_1, subjects_2, subjects_3 = subjects
+number_of_students = ', '.join(students)
+listing_lessons = ', '.join(subjects)
 
-print(
-    f'Students {name_1}, {name_2}, {name_3} study these subjects: '
-    f'{subjects_1}, {subjects_2}, {subjects_3}'
-)
+print(f'Students {number_of_students} '
+      f'study these subjects: {listing_lessons}')
