05ca76990c74268ec362399b6885ce34add363f5
Khusnutdinov Marsel <marsepta806@gmail.com>
2025-10-21 12:09:07 +0300

    Mars_Husnut_HomeWork_8 DONE_2

--- homework/MarsHomeWork/HomeWork_8/bonus.py
+++ homework/MarsHomeWork/HomeWork_8/bonus.py
@@ -3,7 +3,7 @@ import random
 salary = int(input('Введите свою зарплату т.р. '))
 bonus = [True, False]
 
-if random.choice(bonus) == True:
+if random.choice(bonus) is True:
     bonuses = random.randrange(10, 31)
     count = salary // 100 * bonuses + salary
     print(f'Прибавка к вашей зп составит {bonuses}%, итого у Вас {count} т.р.')
