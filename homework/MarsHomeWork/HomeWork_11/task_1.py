e7f963da2619c3ff245221431b5ed986f4d611ec
Khusnutdinov Marsel <marsepta806@gmail.com>
2025-11-16 17:28:37 +0300

    Mars_Husnut_HomeWork_11 Corrected

--- homework/MarsHomeWork/HomeWork_11/task_1.py
+++ homework/MarsHomeWork/HomeWork_11/task_1.py
@@ -2,83 +2,77 @@ class Book:
     text = True
     page_material = 'бумага'
 
-    def __init__(self, title, author, number_of_pages, isbn, meaning="не зарезервирована"):
+    def __init__(self, title, author, number_of_pages, isbn, reserved=False):
         self.title = title
         self.author = author
         self.number_of_pages = number_of_pages
         self.isbn = isbn
-        self.meaning = meaning
+        self.reserved = reserved
+
+    def generation_book(self):
+        if self.reserved:
+            print(f'Название: {self.title}, Автор: {self.author}, '
+                  f'страниц: {self.number_of_pages}, '
+                  f'материал: {self.page_material}, зарезервирована'
+                  )
+
+        else:
+            print(f'Название: {self.title}, Автор: {self.author}, '
+                  f'страниц: {self.number_of_pages}, '
+                  f'материал: {self.page_material}'
+                  )
 
 
 class TextBooks(Book):
     def __init__(self, title, author, number_of_pages, isbn, lesson,
-                 group, availability_of_tasks, meaning="не зарезервирована"
+                 group, availability_of_tasks, reserved=False
                  ):
-        super().__init__(title, author, number_of_pages, isbn, meaning)
+        super().__init__(title, author, number_of_pages, isbn, reserved)
         self.group = group
         self.lesson = lesson
         self.availability_of_tasks = availability_of_tasks
 
+    def generation_text(self):
+        if self.reserved:
+            print(f'Название: {self.title}, Автор: {self.author}, '
+                  f'страниц: {self.number_of_pages}, '
+                  f'предмет: {self.lesson}, класс : {self.group}, зарезервирована'
+                  )
 
-text_books_1 = TextBooks('Алгебра', 'Иванов',
-                         200, '12000444',
-                         'Математика', 9,
-                         True, meaning="зарезервирована"
-                         )
+        else:
+            print(f'Название: {self.title}, Автор: {self.author}, '
+                  f'страниц: {self.number_of_pages}, '
+                  f'предмет: {self.lesson}, класс : {self.group}'
+                  )
 
-text_books_2 = TextBooks('Физика', 'Петров',
-                         10, '12000444',
-                         'Физика', 7, True
-                         )
 
-text_books_3 = TextBooks('Окружающий Мир', 'Иванов',
-                         200, '12000444',
-                         'Биология', 4, True
-                         )
+text_books = [
+    TextBooks('Алгебра', 'Иванов', 200, '12000444',
+              'Математика', 9, True
+              ),
 
-book_1 = Book('Идиот', 'Достоевский', 500, 5423432432,
-              meaning="зарезервирована"
+    TextBooks('Физика', 'Петров', 10,
+              '12000444', 'Физика', 7, True
+              ),
+
+    TextBooks('Окружающий Мир', 'Иванов', 200,
+              '12000444', 'Биология', 4, True
               )
-book_2 = Book('Война и мир', 'Толстой', 100500, 5423432432)
-book_3 = Book('Герой нашего времени', 'Лермонтов', 777, 5423432432)
-book_4 = Book('Мертвые души', 'Горький', 20, 5423432432)
-book_5 = Book('Горе от ума', 'Грибоедов', 299, 5423432432)
+]
 
+books = [
+    Book('Идиот', 'Достоевский', 500, 5423432432),
+    Book('Война и мир', 'Толстой', 100500, 5423432432),
+    Book('Герой нашего времени', 'Лермонтов', 777, 5423432432),
+    Book('Мертвые души', 'Горький', 20, 5423432432),
+    Book('Горе от ума', 'Грибоедов', 299, 5423432432),
+]
 
-def generation_book(text):
-    if text.meaning == "зарезервирована":
-        print(f'Название: {text.title}, Автор: {text.author}, '
-              f'страниц: {text.number_of_pages}, '
-              f'материал: {text.page_material}, {text.meaning}'
-              )
+books[0].reserved = True
+text_books[0].reserved = True
 
-    else:
-        print(f'Название: {text.title}, Автор: {text.author}, '
-              f'страниц: {text.number_of_pages}, '
-              f'материал: {text.page_material}'
-              )
+for book in books:
+    book.generation_book()
 
-
-def generation_text(text):
-    if text.meaning == "зарезервирована":
-        print(f'Название: {text.title}, Автор: {text.author}, '
-              f'страниц: {text.number_of_pages}, '
-              f'предмет: {text.lesson}, класс : {text.group}, {text.meaning}'
-              )
-
-    else:
-        print(f'Название: {text.title}, Автор: {text.author}, '
-              f'страниц: {text.number_of_pages}, '
-              f'предмет: {text.lesson}, класс : {text.group}'
-              )
-
-
-generation_book(book_1)
-generation_book(book_2)
-generation_book(book_3)
-generation_book(book_4)
-generation_book(book_5)
-
-generation_text(text_books_1)
-generation_text(text_books_2)
-generation_text(text_books_3)
+for text in text_books:
+    text.generation_text()
