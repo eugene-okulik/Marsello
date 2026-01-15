5a836f7b50ddb6bd7d560a06c1366cb48bd7d404
Khusnutdinov Marsel <marsepta806@gmail.com>
2026-01-15 17:11:00 +0300

    Mars_Husnut_HomeWork_25 Done

--- homework/MarsHomeWork/HomeWork_25/test_task_1.py
+++ homework/MarsHomeWork/HomeWork_25/test_task_1.py
@@ -1,28 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import pytest
-from time import sleep
 
 
-options = Options()
-options.add_argument('start-maximized')
-driver = webdriver.Chrome(options=options)
-options.add_experimental_option('detach', True)
-driver.find_element()
-driver.find_elements()
-
-
-
-def test_enabled_and_select(driver):
-    driver.get('https://www.qa-practice.com/elements/button/disabled')
-    button = driver.find_element(By.NAME, 'submit')
-    print(button.is_enabled())
-    select = driver.find_element(By.ID, 'id_select_state')
-    dropdown = Select(select)
-    print(button.is_enabled())
+def test_input_text(driver):
+    text = 'hesoyam'
+    driver.get('https://www.qa-practice.com/elements/input/simple')
+    input_text = driver.find_element(By.CSS_SELECTOR, '#id_text_string')
+    input_text.send_keys(text)
+    input_text.submit()
+    result_text = driver.find_element(By.CSS_SELECTOR, '#result-text')
+    assert result_text.text == text
+    print(result_text.text)
