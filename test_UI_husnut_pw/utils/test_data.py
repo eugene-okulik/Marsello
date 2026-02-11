1dd2feac18234e97f1b789c0032a9d8aef663710
Khusnutdinov Marsel <marsepta806@gmail.com>
2026-02-11 19:53:08 +0300

    Mars_Husnut_HomeWork_31 fixed_part_2

--- test_UI_husnut_pw/utils/test_data.py
+++ test_UI_husnut_pw/utils/test_data.py
@@ -1,7 +1,7 @@
 text = 'hesoyam'
-language_python = value="1"
-language_ruby = value="2"
-language_javascript = value="3"
-language_java = value="4"
-language_c_sharp = value="5"
+language_python = value = "1"
+language_ruby = value = "2"
+language_javascript = value = "3"
+language_java = value = "4"
+language_c_sharp = value = "5"
 checked_text = 'Hello World!'
